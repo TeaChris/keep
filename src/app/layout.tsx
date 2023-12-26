d5fcbb61f5bd926d0d51b48e076f29507ee020f4
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-26 12:15:26 +0100

    (feat): navbar set on latge screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { Toaster } from '@/components/ui/toaster'
 import { cn } from '@/lib/utils'
 import { Metadata } from 'next'
 import { siteConfig } from '@/config/site'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,10 +27,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Toaster />
+    <html lang="en" className="h-full">
+      <body
+        className={cn(
+          'relative min-h-screen font-sans antialiased grainy',
+          inter.className
+        )}
+      >
+        <main className="relative flex flex-col min-h-screen">
+          <Navbar />
+          <div className="flex-grow flex-1">{children}</div>
+        </main>
       </body>
     </html>
   )
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { Icons } from './Icons'
+import { buttonVariants } from './ui/button'
+import { ArrowRight } from 'lucide-react'
+
+export default function Navbar() {
+  return (
+    <nav className="sticky flex items-center h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/70 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-full items-center justify-between">
+          <Link href="/" className="flex z-40 font-semibold gap-2 items-center">
+            <Icons.logo className="h-9 w-9" />
+            <h5 className="text-black text-xl">Taskzen</h5>
+          </Link>
+
+          <div className="hidden items-center space-x-4 sm:flex">
+            <>
+              <Link
+                href="/pricing"
+                className={buttonVariants({
+                  variant: 'ghost',
+                  size: 'sm',
+                })}
+              >
+                Pricing
+              </Link>
+              <Link
+                href={'/sign-in'}
+                className={buttonVariants({
+                  variant: 'ghost',
+                  size: 'sm',
+                })}
+              >
+                Sign in
+              </Link>
+
+              <Link
+                href={'/sign-up'}
+                className={buttonVariants({
+                  variant: 'ghost',
+                  size: 'sm',
+                })}
+              >
+                Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+              </Link>
+            </>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  )
+}
