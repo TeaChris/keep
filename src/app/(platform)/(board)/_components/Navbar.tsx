038f715fe4fe7db6200028a572b6858aefce6fb3
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-27 16:53:57 +0100

    (feat): added mobile sidebar

--- src/app/(platform)/(board)/_components/Navbar.tsx
+++ src/app/(platform)/(board)/_components/Navbar.tsx
@@ -4,10 +4,12 @@ import { UserButton } from '@clerk/nextjs'
 import { Icons } from '@/components/Icons'
 import Link from 'next/link'
 import ThemeSwitch from '@/components/ThemeSwitch.tsx'
+import MobileSidebar from './MobileSidebar'
 
 export default function Navbar() {
   return (
     <nav className="px-4 sticky flex items-center h-14 inset-x-0 top-0 w-full border-b border-border bg-background backdrop-blur-lg transition-all">
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <Link href="/" className="flex z-40 font-semibold gap-2 items-center">
           <Icons.logo className="h-9 w-9" />
