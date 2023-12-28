bd87133b33421b33f0f33c4a0bc4b9caf9341c89
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-28 12:43:16 +0100

    (feat): added index file for api

---
+++ src/actions/create-task/index.ts
@@ -0,0 +1,39 @@
+'use server'
+
+import { auth } from '@clerk/nextjs'
+import { InputType, ReturnType } from './types'
+import { db } from '@/lib/db'
+import { revalidatePath } from 'next/cache'
+import { createSafeAction } from '@/lib/create-safe-actions'
+import { CreateTask } from './schema'
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId } = auth()
+
+  if (!userId) {
+    return {
+      error: 'Unauthorized',
+    }
+  }
+
+  const { title } = data
+
+  let task
+
+  try {
+    task = await db.task.create({
+      data: {
+        title,
+      },
+    })
+  } catch (error) {
+    return {
+      error: 'Internal Error',
+    }
+  }
+
+  revalidatePath('/users/dashboard')
+  return { data: task }
+}
+
+export const createTask = createSafeAction(CreateTask, handler)
