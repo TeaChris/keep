8aea46ce629f6f960da8c9f08dac2294fe266319
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-29 10:58:51 +0100

    (feat): task form creation

--- src/app/(platform)/(board)/(users)/users/dashboard/page.tsx
+++ src/app/(platform)/(board)/(users)/users/dashboard/page.tsx
@@ -1,13 +1,28 @@
 import { Separator } from '@/components/ui/separator'
 import Info from './_components/Info'
+import { FormInput } from '@/components/form/form-input'
+import { Search } from 'lucide-react'
+import { TaskList } from './_components/TaskList'
 
 export default async function Dashboard() {
   return (
     <div className="mb-20">
-      <Info />
-      <Separator className="my-4" />
+      <div className="flex items-center justify-between">
+        <Info />
 
-      <div className="px-2 md:px-4">TaskList</div>
+        <div className="w-96 h-14 relative pr-1 text-muted-foreground">
+          <FormInput
+            id="123"
+            placeholder="Enter your search here"
+            className="h-11 rounded-xl pl-10 text-base"
+          />
+          <Search className="w-5 h-5 absolute top-4 left-4" />
+        </div>
+      </div>
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <TaskList />
+      </div>
     </div>
   )
 }
--- src/components/form/form-popover.tsx
+++ src/components/form/form-popover.tsx
@@ -1,101 +1,83 @@
-"use client";
-
-import { ElementRef, useRef } from "react";
-import { toast } from "sonner";
-import { X } from "lucide-react";
-import { useRouter } from "next/navigation";
+'use client'
 
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
   PopoverClose,
-} from "@/components/ui/popover";
-import { useAction } from "@/hooks/use-action";
-import { Button } from "@/components/ui/button";
-import { createBoard } from "@/actions/create-board";
-import { useProModal } from "@/hooks/use-pro-modal";
+} from '../ui/popover'
 
-import { FormInput } from "./form-input";
-import { FormSubmit } from "./form-submit";
-import { FormPicker } from "./form-picker";
+import { useAction } from '@/hooks/use-action'
+import { createTask } from '@/actions/create-task'
+
+import { FormInput } from './form-input'
+import { FormSubmit } from './form-submit'
+import { Button } from '../ui/button'
+import { X } from 'lucide-react'
+import { toast } from 'sonner'
 
 interface FormPopoverProps {
-  children: React.ReactNode;
-  side?: "left" | "right" | "top" | "bottom";
-  align?: "start" | "center" | "end";
-  sideOffset?: number;
-};
+  children: React.ReactNode
+  side?: 'left' | 'right' | 'top' | 'bottom'
+  align?: 'start' | 'center' | 'end'
+  sideOffset?: number
+}
 
-export const FormPopover = ({
+export default function FormPopover({
   children,
-  side = "bottom",
+  side = 'bottom',
   align,
   sideOffset = 0,
-}: FormPopoverProps) => {
-  const proModal = useProModal();
-  const router = useRouter();
-  const closeRef = useRef<ElementRef<"button">>(null);
-
-  const { execute, fieldErrors } = useAction(createBoard, {
+}: FormPopoverProps) {
+  const { execute, fieldErrors } = useAction(createTask, {
     onSuccess: (data) => {
-      toast.success("Board created!");
-      closeRef.current?.click();
-      router.push(`/board/${data.id}`);
+      console.log({ data })
+      toast.success('Task created')
     },
     onError: (error) => {
-      toast.error(error);
-      proModal.onOpen();
-    }
-  });
+      console.error({ error })
+      toast.error(error)
+    },
+  })
 
   const onSubmit = (formData: FormData) => {
-    const title = formData.get("title") as string;
-    const image = formData.get("image") as string;
+    const title = formData.get('title') as string
 
-    execute({ title, image });
+    execute({ title })
   }
 
   return (
     <Popover>
-      <PopoverTrigger asChild>
-        {children}
-      </PopoverTrigger>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
       <PopoverContent
         align={align}
         className="w-80 pt-3"
         side={side}
         sideOffset={sideOffset}
       >
-        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
-          Create board
+        <div className="text-sm font-medium text-center text-muted-foreground pb-4">
+          Create task
         </div>
-        <PopoverClose ref={closeRef} asChild>
+        <PopoverClose asChild>
           <Button
-            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
-            variant="ghost"
+            className="h-auto w-auto absolute p-2 top-2 right-2 text-muted-foreground"
+            variant={'ghost'}
           >
             <X className="h-4 w-4" />
           </Button>
         </PopoverClose>
-        <form action={onSubmit} className="space-y-4">
+        <form className="space-y-4" action={onSubmit}>
           <div className="space-y-4">
-            <FormPicker
-              id="image"
-              errors={fieldErrors}
-            />
             <FormInput
               id="title"
-              label="Board title"
+              label="Task title"
               type="text"
               errors={fieldErrors}
             />
           </div>
-          <FormSubmit className="w-full">
-            Create
-          </FormSubmit>
+          <FormSubmit className="w-full">Create</FormSubmit>
         </form>
       </PopoverContent>
     </Popover>
-  );
-};
+  )
+}
